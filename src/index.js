c10a344882f1c04ecaab97264a9be1c5f9d82b63
hemanth krishna <k.hemanth1999@gmail.com>
2022-09-08 14:33:10 +0530

    finished weather app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
 	state = { lat: null, errorMessage: '' }
@@ -9,14 +11,21 @@ class App extends React.Component {
 			(err) => this.setState({ errorMessage: err.message })
 		)
 	}
-	render() {
+	renderContent() {
 		if (this.state.errorMessage && !this.state.lat) {
 			return <div>Error: {this.state.errorMessage} </div>
 		}
 		if (!this.state.errorMessage && this.state.lat) {
-			return <div>Latitude: {this.state.lat} </div>
+			return <SeasonDisplay lat={this.state.lat} />
 		}
-		return (<div> Loading</div>)
+		return <Spinner message="Please accept location request" />
+	}
+	render() {
+		return (
+			<div className="border red">
+				{this.renderContent()}
+			</div>
+		)
 	}
 
 }
