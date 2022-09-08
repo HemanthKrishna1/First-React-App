69509b1b32b1d5da782e02e6b66cbd0aa5f9ba3b
hemanth krishna <k.hemanth1999@gmail.com>
2022-09-08 13:41:05 +0530

    alternate state

--- src/index.js
+++ src/index.js
@@ -2,20 +2,14 @@ import React from "react";
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { lat: null, errorMessage: '' }
-	}
-
-	render() {
+	state = { lat: null, errorMessage: '' }
+	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
-			(position) => {
-				this.setState({ lat: position.coords.latitude })
-			},
-			(err) => {
-				this.setState({ errorMessage: err.message })
-			}
+			(position) => this.setState({ lat: position.coords.latitude }),
+			(err) => this.setState({ errorMessage: err.message })
 		)
+	}
+	render() {
 		if (this.state.errorMessage && !this.state.lat) {
 			return <div>Error: {this.state.errorMessage} </div>
 		}
